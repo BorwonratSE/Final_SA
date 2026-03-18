ef948534967a9b17f067422dd0be19c4b68a5f3f
Bas <panuwat_ya67@live.rmutl.ac.th>
2026-03-18 12:47:10 +0700

    chore: add jwtUtils.js to follow strict project structure

--- final-lab-sec2-set2-[67543210036-9]-[67543210043-5]/auth-service/src/middleware/jwtUtils.js
+++ final-lab-sec2-set2-[67543210036-9]-[67543210043-5]/auth-service/src/middleware/jwtUtils.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken');
-const SECRET  = process.env.JWT_SECRET  || 'dev-secret';
-const EXPIRES = process.env.JWT_EXPIRES || '1h';
+const JWT_SECRET = process.env.JWT_SECRET || 'dev-secret';
 
+// ฟังก์ชันสำหรับสร้าง Token (ใช้ใน Auth Service)
 function generateToken(payload) {
-  return jwt.sign(payload, SECRET, { expiresIn: EXPIRES });
+  // ตาม docker-compose อาจารย์ตั้งให้หมดอายุใน 1h
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }); 
 }
 
+// ฟังก์ชันสำหรับตรวจ Token (ใช้ใน Task Service)
 function verifyToken(token) {
-  return jwt.verify(token, SECRET);
+  return jwt.verify(token, JWT_SECRET);
 }
 
 module.exports = { generateToken, verifyToken };
