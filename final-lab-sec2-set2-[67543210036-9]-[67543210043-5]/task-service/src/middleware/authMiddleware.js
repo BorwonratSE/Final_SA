b3ea82d979e0aa724a375508f6e8d4d9b2c07925
BorwonratSE <borwonrat_si67@live.rmutl.ac.th>
2026-03-18 12:50:52 +0700

    task-service/src/middleware/jwtUtils.js

--- final-lab-sec2-set2-[67543210036-9]-[67543210043-5]/task-service/src/middleware/authMiddleware.js
+++ final-lab-sec2-set2-[67543210036-9]-[67543210043-5]/task-service/src/middleware/authMiddleware.js
@@ -1,11 +1,10 @@
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET || 'dev-secret';
+const { verifyToken } = require('./jwtUtils');
 
 module.exports = (req, res, next) => {
   const token = (req.headers['authorization'] || '').split(' ')[1];
   if (!token) return res.status(401).json({ error: 'Unauthorized' });
   try {
-    req.user = jwt.verify(token, JWT_SECRET);
+    req.user = verifyToken(token); // เรียกใช้จาก jwtUtils
     next();
   } catch (err) { res.status(401).json({ error: 'Invalid token' }); }
 };
